f96809e8b3164ed837b9f1b776717642ce9ccf01
Lucas Macedo <luuckymacedo@gmail.com>
2017-08-18 17:46:34 -0300

    Part 1 — Database Seeding

---
+++ database/migrations/2017_08_18_193955_create_films_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug')->nullable();
+            $table->date('release');
+            $table->string('locale');
+            $table->integer('duration');
+            $table->longText('sinopse');
+            $table->string('site')->nullable();
+            $table->string('cover');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
---
+++ database/seeds/FilmsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Film;
+class FilmsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+			$faker = Faker::create('pt_BR');
+
+           foreach (range(1,20) as $index) {
+           	$name = $faker->name;
+           	
+	           Film::create([
+	               	'name' => $name,
+	               	'slug' => str_slug($name),
+		            'release' => $faker->date('d/m/Y'),
+		            'locale' => $faker->city . ' - ' . $faker->stateAbbr . ', ' . $faker->country,
+		            'site' =>  $faker->url,
+		            'duration' =>  $faker->numberBetween(40,200),
+		            'sinopse' =>  $faker->sentence,
+		            'cover' =>   $faker->imageUrl,
+	           ]);
+
+          }
+    }
+}
